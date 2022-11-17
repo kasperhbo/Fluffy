83e40cfab85a30b4f015d3588fcf7ea1937ed04a
kaspoehh <kasperbruin8@gmail.com>
2022-11-17 17:04:35 +0100

    Changed default Windows SDK version to always set to latest

--- premake5.lua
+++ premake5.lua
@@ -32,7 +32,7 @@ project "Fluffy"
 	filter "system:windows"
 		cppdialect "C++Latest"
 		staticruntime "On"
-		systemversion "10.0.17134.0"
+		systemversion "latest"
 
 		defines
 		{
@@ -85,7 +85,7 @@ project "Sandbox"
 	filter "system:windows"
 		cppdialect "C++17"
 		staticruntime "On"
-		systemversion "10.0.17134.0"
+		systemversion "latest"
 
 		defines
 		{
